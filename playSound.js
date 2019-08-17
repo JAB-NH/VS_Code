e1910cc100887625e4285b65c40a4521e1336dec
James <j-a-bates@rogers.com>
2019-08-16 23:51:59 -0400

    Modified Ants and Songs

--- playSound.js
+++ playSound.js
@@ -12,7 +12,7 @@ function preload() {
   // we have included with our sketch
  // song = loadSound("watchtower.mp3");
   song1 = loadSound("On The Streets of Amsterdam.wav");
-  song2 = loadSound("watchtower.mp3")
+  song2 = loadSound("One_Day.wav")
   
 }
 
@@ -20,8 +20,9 @@ function setup() {
   canvas = createCanvas(50, 50);
 
 //slider=createSlider(0,1,.3,0.01);
-  //createElement('p');
-  //button=createButton("Click to Play")
+  //createElement('br');
+  //createElement('br');
+  button=createButton("Click to Play")
 
  // button.mousePressed("play")
   // CANNOT NCLUDE THE NEXT LINE     
@@ -41,12 +42,12 @@ function mousePressed(){
     // .isPlaying() returns a boolean
     song2.pause();
     song1.play();
-    canvas.position(50,200);
+    canvas.position(50,170);
     background(0,255, 0);
   } else {
     song1.pause(); // playback will resume from the pause position
     song2.play();
-    canvas.position(200,200);
+    canvas.position(250,170);
     background(255,0, 0);
   }
 }
